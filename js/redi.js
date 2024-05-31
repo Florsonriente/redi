5b3837a359aac90441ca5f381c9f2fa0c3114f07
Andrii Kalko <andrii.kalko@casavi.de>
2024-05-31 08:00:06 +0200

    air quality api insert city in h2

--- js/redi.js
+++ js/redi.js
@@ -384,52 +384,77 @@ buttonGetSavedCarbon.addEventListener("click", getSavedCarbon);
     activities.addEventListener("input", getSavedCarbon);
   });*/
 
-////////////////////////////////// AIR QUALITY
 
-const form = document.getElementById("city-form");
-const resultDiv = document.getElementById("result_coordinates");
+
+//AIR QUALITY GOT FROM THE CITY from input form WRITTEN BY THE USER: 3 steps:
+// 1) STEP 1: GET THE lat and lon coordinates (for API url) from the city input from the user
+// 2) STEP 2: integrate the lat and lon from the city into API Key url and get the air quality 
+// 3) STEP 3 : do the styling of the needle, of the clouds and the h2 content describing the air quality 
+
+// STEP 1: GET THE lat and lon coordinates (for API url) from the city input from the user
+
+
+//STEP 1a:   /* Converts a city name to latitude and longitude coordinates using the OpenStreetMap Nominatim API, the first city from the object: */
+
+
 
 async function getCoordinates(city) {
-  /*
-   * Converts a city name to latitude and longitude coordinates using the OpenStreetMap Nominatim API.
-   * Logs the retrieved data regardless of the length.
-   */
+
   const url = `https://nominatim.openstreetmap.org/search?q=${city}&format=json`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data); // Log the data regardless of the length
+  console.log(data); // Log the data 
   if (data.length > 0) {
-    return { lat: parseFloat(data[0].lat), lon: parseFloat(data[0].lon) };
+    // sort by importance 
+    return { 
+        lat: parseFloat(data[0].lat), 
+        lon: parseFloat(data[0].lon), 
+        display_name: data[0].display_name
+         };
   } else {
     return null;
   }
 }
 
+//STEP 2a: integrates the data from the API into the url that we need for our Air Quality API url 
+
+const form = document.getElementById("city-form");
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
 
+  let locationName;
   const cityInput = document.getElementById("city-input");
   const city = cityInput.value;
+  const aqiDescription = document.getElementById("airquality_description");
 
   const location = await getCoordinates(city);
   if (location) {
     const lat = location.lat;
     const lon = location.lon;
+    locationName = location.display_name;
 
     // Construct the URL with the actual latitude and longitude
     const url = `https://cors-anywhere.herokuapp.com/https://api.ambeedata.com/latest/by-lat-lng?lat=${lat}&lng=${lon}`;
+ 
 
     // Call the API with the updated URL
-    fetchAndUpdateAirQuality(url);
+    fetchAndUpdateAirQuality(url, locationName);
+    //aqiDescription.textContent = locationName;
   } else {
-    resultDiv.textContent = "Unable to find coordinates for the given city.";
+    aqiDescription.textContent = "Unable to find coordinates for the given city. Please try again!";
+  
   }
 });
 
-function fetchAndUpdateAirQuality(url) {
+//STEP 2b: calls our Air Quality API with correct url
+
+function fetchAndUpdateAirQuality(url, locationName) {
+
+    
   // Define your API key
   const api_key =
-    "1b5d859b6824861a56ffd2a5d3a1a827705605b7e6d91dd98333914220dcf651";
+    //"1b5d859b6824861a56ffd2a5d3a1a827705605b7e6d91dd98333914220dcf651";
+    "bf5065e8187b79be1d6e99b3ec2823ff430f81079d4e9af613967b6e71a975b9";
 
   // Set headers with your API key
   const headers = {
@@ -450,7 +475,7 @@ function fetchAndUpdateAirQuality(url) {
     .then((data) => {
       const airQuality = data?.stations?.[0]?.AQI;
       if (airQuality !== undefined) {
-        updateGauge(airQuality);
+        updateAirQualityCloudGaugeDescription(airQuality, locationName);
       } else {
         throw new Error("Invalid data received from the API");
       }
@@ -458,23 +483,21 @@ function fetchAndUpdateAirQuality(url) {
     .catch((error) => console.error("Error fetching air quality data:", error));
 }
 
+// 3) STEP 3 : do the styling of the needle, of the clouds and the h2 content describing the air quality 
+
 // Function to update the air quality gauge
-function updateGauge(aqi) {
+function updateAirQualityCloudGaugeDescription(aqi, locationName) {
   const needle = document.querySelector(".needle");
   const aqiSpan = document.getElementById("aqi");
   const aqiDescription = document.getElementById("airquality_description");
+  
 
   // Rotate the needle based on the AQI value
   const rotation = (aqi / 500) * 180; // Assuming AQI ranges from 0 to 500
   needle.style.transform = `rotate(${rotation}deg)`;
-
   aqiSpan.textContent = `${aqi}`;
-  //  aqiDescription.textContent = `${aqi}`;
 
-  // Log aqiSpan for testing purposes
-  console.log(aqiSpan);
-
-  ////// AIR QUALITY CLOUDS STYLING
+  ////// Function to update the clouds and the h2 text content based on the got from the Air Quality data
 
   let clouds = document.getElementById("image_wrapper");
 
@@ -484,26 +507,26 @@ function updateGauge(aqi) {
 
   if (aqi <= 50) {
     clouds.style.filter = "invert(0) brightness(1) blur(9px)";
-    aqiDescription.textContent = `The air quality index is ${aqi} and it means that the air quality is good. Nevertheless we can still think about the ways to amke it better!`;
+    aqiDescription.textContent = `The air quality index in ${locationName} is ${aqi} and it means that the air quality is good. Nevertheless we can still think about the ways to make it better!`;
   } else if (aqi <= 100) {
     clouds.style.filter = "invert(0) brightness(0.5) blur(9px)";
-    aqiDescription.textContent = `The air quality index is ${aqi} and it means that the air quality is moderate. Usually sensitive individuals should consider limiting prolonged outdoor exertion.`;
+    aqiDescription.textContent = `The air quality index in ${locationName} is ${aqi} and it means that the air quality is moderate. Usually sensitive individuals should consider limiting prolonged outdoor exertion.`;
   } else if (aqi <= 150) {
     clouds.style.filter = "invert(0) brightness(0.3) blur(9px)";
-    aqiDescription.textContent = `The air quality index is ${aqi} and it means that the air quality is Unhealthy for Sensitive Groups. Children, active adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion.`;
+    aqiDescription.textContent = `The air quality index in ${locationName} is ${aqi} and it means that the air quality is Unhealthy for Sensitive Groups. Children, active adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion.`;
   } else if (aqi <= 200) {
     clouds.style.filter = "invert(1) brightness(0)";
-    aqiDescription.textContent = `The air quality index is ${aqi} and it means that the air quality is Unhealthy. Children, active adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion.`;
+    aqiDescription.textContent = `The air quality index in ${locationName} is ${aqi} and it means that the air quality is Unhealthy. Children, active adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion.`;
   } else if (aqi <= 300) {
     clouds.style.filter = "invert(1) brightness(0)";
-    aqiDescription.textContent = `The air quality index is ${aqi} and it means that the air quality is Very Unhealthy. Children, active adults, and people with respiratory disease, such as asthma, should avoid outdoor exertion; everyone else should limit outdoor exertion.`;
+    aqiDescription.textContent = `The air quality index in ${locationName} is ${aqi} and it means that the air quality is Very Unhealthy. Children, active adults, and people with respiratory disease, such as asthma, should avoid outdoor exertion; everyone else should limit outdoor exertion.`;
   } else if (aqi <= 500) {
     clouds.style.filter = "invert(1) brightness(0)";
-    aqiDescription.textContent = `The air quality index is ${aqi} and it means that the air quality is Hazardous. Everyone should avoid all physical activity outdoors.`;
+    aqiDescription.textContent = `The air quality index in ${locationName} is ${aqi} and it means that the air quality is Hazardous. Everyone should avoid all physical activity outdoors.`;
   }
 }
 
-////// TO DO LIST
+////// TO DO LIST/ THOUGHTS LIST
 
 const inputBox = document.getElementById("input_box");
 const listContainer = document.getElementById("list_container");
@@ -603,6 +626,8 @@ function toDoListBackgroundsSelector() {
   const index = Math.floor(Math.random() * toDoListBackgrounds.length);
   return toDoListBackgrounds[index];
 }
+
+
 
 ////COOL CURSOR
 var smallCircleCursor = document.getElementById("small_circle_cursor");
